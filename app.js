98078e6aabe40127e5bd5ea8327338a507369cee
FaheemBuksh-FB <72087381+FaheemBuksh-FB@users.noreply.github.com>
2020-11-07 07:43:47 -0800

    First

---
+++ app.js
@@ -0,0 +1,24 @@
+var cnt = 0
+var lnt=$('.cimg').length;
+$(document).ready(()=> {
+    // var lnt=$('.cimg').length;
+    alert(lnt);
+    // $('#nxt').click(function(){
+    //     // alert("AF");
+    //     if(abc>0){
+    //     abc++
+    //     $('.cimg:eq('+(abc-1)+')').css('display','none');
+    //     $('.cimg:eq('+abc+')').css('display','block');
+    //     }
+    //     else{
+    //     abc=1
+    //     $('.cimg:eq('+(abc-1)+')').css('display','none');
+    //     $('.cimg:eq('+abc+')').css('display','block'); 
+    //     }
+    // });
+    // $('#prev').click(function(){
+    //     abc--
+    //     $('.cimg:eq('+abc+')').css('display','none');
+    //     $('.cimg:eq('+(abc-1)+')').css('display','block');    
+    // });
+});
